d341cbf776f437c8a3f0477c6a87d08118c79905
lucasfagundesgweb <lucasfagundes.gweb@gmail.com>
2025-11-30 16:12:47 -0300

    enemy death animation

--- DTJ_3620_Project/Assets/Scripts/Health/EnemyHealth.cs
+++ DTJ_3620_Project/Assets/Scripts/Health/EnemyHealth.cs
@@ -1,16 +1,25 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class EnemyHealth : HealthBehaviour
 {
+    [Header("// DEATH ANIMATION")]
+    [SerializeField] SpriteRenderer _sprite = null;
+    [SerializeField] float _duration = 1f;
+
     public static event UnityAction<HealthBehaviour> OnEnemyDie = null;
 
     protected override void OnDead_()
     {
         base.OnDead_();
         //gameObject.SetActive(false);
-        Destroy(gameObject);
-        // fade and then destroy.
+        //Destroy(gameObject);
+
+        _sprite.color = Color.red;
+        _sprite.DOFade(0, _duration).
+            OnComplete(() => Destroy(gameObject));
+
         OnEnemyDie?.Invoke(this);
     }
 }
