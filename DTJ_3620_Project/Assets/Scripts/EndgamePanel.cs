8850718ddeb3b4aedc22890e077271543868a540
lucasfagundesgweb <lucasfagundes.gweb@gmail.com>
2025-11-30 18:31:58 -0300

    lots of changes

--- DTJ_3620_Project/Assets/Scripts/EndgamePanel.cs
+++ DTJ_3620_Project/Assets/Scripts/EndgamePanel.cs
@@ -1,7 +1,9 @@
+using System.Collections;
 using UnityEngine;
 
 public class EndgamePanel : Singleton<EndgamePanel>
 {
+    [SerializeField] CanvasView _flash = null;
     [SerializeField] CanvasView _victoryPanel = null;
     [SerializeField] CanvasView _defeatPanel = null;
 
@@ -9,13 +11,20 @@ public class EndgamePanel : Singleton<EndgamePanel>
     {
         if (_isVictory)
         {
-            _victoryPanel.Show();
-            // play victory music
+            StartCoroutine(Victory_Routine());
         }
         else
         {
             _defeatPanel.Show();
             // play defeat music.
         }
+    }
+
+    IEnumerator Victory_Routine()
+    {
+        _flash.Show();
+        yield return new WaitForSeconds(2);
+        _victoryPanel.Show();
+        // play victory music
     }
 }
